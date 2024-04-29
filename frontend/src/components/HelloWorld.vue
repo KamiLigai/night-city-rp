1065481ac96765775ce0ec6199e8adadce966d9f
Nikita Trubkin <trubkin.nikita@yandex.ru>
2024-04-29 18:24:36 +0300

    #2 Добавлен frontend на vuetify (#11)

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Night City RP</h1>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <HelloWorld class="hello-world" />
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
+
+<style lang="scss" scoped>
+.hello-world{
+  margin: 24px;
+}
+</style>
