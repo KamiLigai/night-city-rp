53a1086132e6692ae64ce0e770dc76e22ab4d421
ntrubkin <trubkin.nikita@yandex.ru>
2024-05-10 19:23:02 +0300

    #5 Исправлено появление дефолтной формы логина и base64

--- frontend/src/views/HeaderView.vue
+++ frontend/src/views/HeaderView.vue
@@ -2,33 +2,35 @@
 import { useUserStore } from '@/stores/userStore'
 import router from '@/router'
 import client from '@/Clients/Client'
+import { onMounted } from 'vue'
+import { Base64 } from 'js-base64'
 
 const userStore = useUserStore()
 
 function logout() {
-  client.logout()
-  router.push('login')
+    client.logout()
+    router.push('login')
 }
 
 </script>
 
 <template>
-  <div class="container">
-    <h1>Night City RP</h1>
-    <router-link :to="{name: 'home'}">Главная</router-link>
-    <router-link v-if="!userStore.user?.username" :to="{name: 'login'}">Войти</router-link>
-    <router-link v-if="!userStore.user?.username" :to="{name: 'registration'}">Зарегистрироваться</router-link>
-    <p v-if="userStore.user?.username">{{ userStore.user?.username }}</p>
-    <button v-if="userStore.user?.username" v-on:click="logout">Выйти</button>
-  </div>
+    <div class="container">
+        <h1>Night City RP</h1>
+        <router-link :to="{name: 'home'}">Главная</router-link>
+        <router-link v-if="!userStore.user?.username" :to="{name: 'login'}">Войти</router-link>
+        <router-link v-if="!userStore.user?.username" :to="{name: 'registration'}">Зарегистрироваться</router-link>
+        <p v-if="userStore.user?.username">{{ userStore.user?.username }}</p>
+        <button v-if="userStore.user?.username" v-on:click="logout">Выйти</button>
+    </div>
 
 </template>
 
 <style scoped>
 .container {
-  display: flex;
-  flex-direction: row;
-  gap: 24px;
+    display: flex;
+    flex-direction: row;
+    gap: 24px;
 }
 
 </style>
