0ad2f6fe219d3f42fafd2eef482fe9054a549f2b
Nikita Trubkin <trubkin.nikita@yandex.ru>
2024-06-10 21:33:23 +0300

    #29 Добавлены алерты на фронтенд (#38)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,10 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Vue3Toastify, { toast, type ToastContainerOptions } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Vue3Toastify, {
+    autoClose: 5000,
+    position: toast.POSITION.BOTTOM_RIGHT
+} as ToastContainerOptions);
 
 app.mount('#app')
--- frontend/src/views/CharacterView.vue
+++ frontend/src/views/CharacterView.vue
@@ -4,8 +4,9 @@ import { onMounted, ref } from 'vue'
 import { CharacterDto } from '@/dto/CharacterDto'
 import client from '@/Clients/Client'
 import { useRoute } from 'vue-router'
+import { toast } from 'vue3-toastify'
 
-const route = useRoute();
+const route = useRoute()
 const character = ref<CharacterDto>()
 
 onMounted(() => {
@@ -13,14 +14,14 @@ onMounted(() => {
     const id = route.params.characterId as string
     client.getCharacter(id)
         .then(response => character.value = response.data)
-        .catch(() => console.error("request failed"))
+        .catch(() => toast('Ошибка запроса персонажа', { type: toast.TYPE.ERROR }))
 })
 </script>
 
 <template>
     <h1>Персонаж</h1>
-    <p>Имя: {{character?.name}}</p>
-    <p>Возраст: {{character?.age}}</p>
+    <p>Имя: {{ character?.name }}</p>
+    <p>Возраст: {{ character?.age }}</p>
     <p>Владелец: -</p>
 </template>
 
