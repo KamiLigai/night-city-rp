88e1e24a2d519fa547ab1d5852443bafb86ed6b7
KamiLigai <168115138+KamiLigai@users.noreply.github.com>
2024-06-15 19:59:25 +0400

    #4 Внедрён Checkstyle (#39)
    * #4 Внедрён Checkstyle
    
    * #4 Внедрён Checkstyle

--- backend/src/main/java/ru/nightcityroleplay/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/ru/nightcityroleplay/backend/config/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package ru.nightcityroleplay.backend.config;
 
 import lombok.SneakyThrows;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -14,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.Customizer.withDefaults;
 
