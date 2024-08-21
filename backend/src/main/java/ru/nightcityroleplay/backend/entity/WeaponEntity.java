679b3b8ede1307587cbbafa788d12b624ca26ba1
Roman Akhmedjanov <MeowEasyRG@yandex.ru>
2024-08-21 22:04:16 +0400

    #47 Создана Сущность

--- backend/src/main/java/ru/nightcityroleplay/backend/entity/WeaponEntity.java
+++ backend/src/main/java/ru/nightcityroleplay/backend/entity/WeaponEntity.java
@@ -1,4 +1,23 @@
 package ru.nightcityroleplay.backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+@Entity
+@Table(name = "weapons")
+@Setter
+@Getter
+
 public class WeaponEntity {
+    private int id;
+    private Boolean is_melee;
+    private String name;
+    private String weapon_type;
+    private int penetration;
+    private int reputation_requirement;
+
 }
