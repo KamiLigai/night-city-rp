b28301c55f90b7824297982b7428ebb7c0832c48
Roman Akhmedjanov <MeowEasyRG@yandex.ru>
2024-09-12 19:41:53 +0400

    #7 Исправлен UserTest

--- backend/src/test/java/ru/nightcityroleplay/backend/entity/UserTest.java
+++ backend/src/test/java/ru/nightcityroleplay/backend/entity/UserTest.java
@@ -6,23 +6,21 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UserTest {
-
     @ParameterizedTest
     @MethodSource("getAuthoritiesData")
     void getAuthorities(List<String> roles, List<String> expectedAuthorities) {
         // given
         var user = new User();
-        Set<Role> rolesEntities = roles.stream()
+        List<Role> rolesEntities = roles.stream()
             .map(this::createRole)
-            .collect(toSet());
+            .collect(toList());
         user.setRoles(rolesEntities);
 
         // when
