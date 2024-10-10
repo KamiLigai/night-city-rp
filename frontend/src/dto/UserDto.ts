9450afb0f4055eb52509238a3f06b141314125c9
Nikita Trubkin <trubkin.nikita@yandex.ru>
2024-10-10 22:46:39 +0300

    #62 Добавлена страница текущего юзера (#74)
    * #62 Добавлена страница текущего юзера
    
    * #62 Добавлен алерт регистрации

--- frontend/src/dto/UserDto.ts
+++ frontend/src/dto/UserDto.ts
@@ -1,9 +1,11 @@
 export class UserDto {
   id: string;
   username: string;
+  roles: string[]
 
-  constructor(id: string, username: string) {
+  constructor(id: string, username: string, roles: string[]) {
     this.id = id
     this.username = username
+    this.roles = roles
   }
 }
