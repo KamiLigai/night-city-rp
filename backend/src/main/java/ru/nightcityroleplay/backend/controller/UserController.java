789f448dc4a151ed982aed67e2d82cc68ef868e8
Nikita Trubkin <trubkin.nikita@yandex.ru>
2024-10-10 23:05:26 +0300

    #57 Добавлен мониторинг

--- backend/src/main/java/ru/nightcityroleplay/backend/controller/UserController.java
+++ backend/src/main/java/ru/nightcityroleplay/backend/controller/UserController.java
@@ -2,11 +2,7 @@ package ru.nightcityroleplay.backend.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import ru.nightcityroleplay.backend.dto.CreateUserRequest;
 import ru.nightcityroleplay.backend.dto.UserDto;
 import ru.nightcityroleplay.backend.service.UserService;
