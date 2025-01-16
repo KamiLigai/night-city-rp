7bb51a7fda0ea761aa9506130ec2dd62cbe8123b
Roman Akhmedjanov <MeowEasyRG@yandex.ru>
2025-01-16 19:30:08 +0400

    #98 Правки для Ревью

--- backend/src/main/java/ru/nightcityroleplay/backend/dto/UpdateCharacterImplantsRequest.java
+++ backend/src/main/java/ru/nightcityroleplay/backend/dto/UpdateCharacterImplantsRequest.java
@@ -1,13 +1,11 @@
 package ru.nightcityroleplay.backend.dto;
 
 import lombok.Data;
-import lombok.Getter;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
 public class UpdateCharacterImplantsRequest {
-    @Getter
     private List<UUID> implantIds;
 }
