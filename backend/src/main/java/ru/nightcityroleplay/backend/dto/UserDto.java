9ee39ad94a91071a93f5206151b3d836b1b81413
Roman Akhmedjanov <MeowEasyRG@yandex.ru>
2025-01-19 19:24:49 +0400

    #85

--- backend/src/main/java/ru/nightcityroleplay/backend/dto/UserDto.java
+++ backend/src/main/java/ru/nightcityroleplay/backend/dto/UserDto.java
@@ -1,11 +1,14 @@
 package ru.nightcityroleplay.backend.dto;
 
 import lombok.Builder;
+
+import java.util.List;
 import java.util.UUID;
 
 public record UserDto(
     UUID id,
-    String username
+    String username,
+    List<String> roles
 ) {
     @Builder
     public UserDto {
