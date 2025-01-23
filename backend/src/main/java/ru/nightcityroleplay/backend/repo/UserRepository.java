33060ec9a0463ee7fa5e176cba332c01e21e2380
KamiLigai <168115138+KamiLigai@users.noreply.github.com>
2025-01-23 19:29:59 +0400

    #85 Добавил эндпоинты получения списка юзеров и конкретного юзера по id (#110)
    * #85 Добавил эндпоинты получения списка юзеров и конкретного юзера по id
    
    * #85 Добавил эндпоинты получения списка юзеров и конкретного юзера по id
    
    * #85 Правки для Ревью
    
    * #85 Правки для Ревью
    
    * #85 Правки для Ревью
    
    * #85
    
    * #85 Правки для Ревью
    
    * #85 Правки для Ревью

--- backend/src/main/java/ru/nightcityroleplay/backend/repo/UserRepository.java
+++ backend/src/main/java/ru/nightcityroleplay/backend/repo/UserRepository.java
@@ -1,12 +1,12 @@
 package ru.nightcityroleplay.backend.repo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nightcityroleplay.backend.entity.User;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByUsername(String username);
 }
